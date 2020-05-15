80dceba2ce87fc0a769b843b22ddb179aa1b29b0
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-05-15 15:40:11 +0200

    chore(install): use different workdir

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 nice cargo build --release
 sudo cp -uv target/release/network-stalker /usr/local/bin/
 
+sudo mkdir -p /var/cache/network-stalker
+
 # copy stuff
 sudo cp -uv *.service /etc/systemd/system
 
