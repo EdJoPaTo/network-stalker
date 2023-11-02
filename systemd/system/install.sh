193470db21a397214d0d152ece3e917f18c001a5
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-11-02 15:44:35 +0100

    chore(systemd): improve service and install script

--- systemd/system/install.sh
+++ systemd/system/install.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
-set -e
-
-name="network-stalker"
+set -eu
 
 dir=$(basename "$(pwd)")
 if [ "$dir" == "systemd" ] || [ "$dir" == "system" ]; then
-    echo "run from main directiory like this: ./systemd/system/install.sh"
-    exit 1
+	echo "run from main directiory like this: ./systemd/system/install.sh"
+	exit 1
 fi
 
 nice cargo build --release --locked
 
 # systemd
-sudo mkdir -p /usr/local/lib/systemd/system/
-sudo cp -v "systemd/system/service" "/usr/local/lib/systemd/system/$name.service"
+function copyIntoLocal() {
+	sudo mkdir -p "$(dirname "$2")"
+	sed -e 's#/usr/#/usr/local/#' -e 's#/var/#/var/local/#' "$1" | sudo tee "$2" >/dev/null
+}
+copyIntoLocal systemd/system/service "/usr/local/lib/systemd/system/network-stalker.service"
 sudo systemctl daemon-reload
 
 # stop, replace and start new version
-sudo systemctl stop "$name.service"
-sudo cp -v "target/release/$name" /usr/local/bin
+sudo systemctl stop "network-stalker.service"
+sudo cp -v "target/release/network-stalker" /usr/local/bin/
 
-sudo systemctl enable --now "$name.service"
+sudo systemctl enable --now "network-stalker.service"
--- systemd/system/remove.sh
+++ systemd/system/remove.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
-name="network-stalker"
+sudo systemctl disable --now "network-stalker.service"
 
-sudo systemctl disable --now "$name.service"
-
-sudo rm -f "/usr/local/lib/systemd/system/$name.service"
-sudo rm -f "/usr/local/bin/$name"
-
-sudo userdel -r "$name"
-sudo groupdel "$name"
+sudo rm -f "/usr/local/lib/systemd/system/network-stalker.service"
+sudo rm -f "/usr/local/bin/network-stalker"
 
 sudo systemctl daemon-reload
-
-
-echo "/var/local/lib/network-stalker/ is not touched and is still existing"
